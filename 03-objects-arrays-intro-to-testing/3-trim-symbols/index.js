64c66bb7f2760deee9665faa441906c2e127ccde
disazonov <sazonovdv01@gmail.com>
2025-02-09 20:50:54 +0100

    trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,34 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
 
+export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
+  if (size === 0) {
+    return '';
+  }
+
+  let result = '';
+  let count = 0;
+  let lastChar = '';
+
+  for (let char of string) {
+    if (char === lastChar) {
+      if (count < size) {
+        result += char;
+        count++;
+      }
+    } else {
+      result += char;
+      lastChar = char;
+      count = 1;
+    }
+  }
+
+  return result;
 }
+
+
+
